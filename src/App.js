f308f59bb44df8734c1149af2656829b48cdeedb
billie-badger <will.vatcher@gmail.com>
2018-07-03 21:22:23 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>EXTREME TIME HEROES</h1>
+        <h2>FIGHT FOR YOUR TIME</h2>
+        <a href="mailto:team@finalitylabs.com"><h4>team@finalitylabs.com</h4></a>
+      </div>
+    );
+  }
+}
+
+export default App;
