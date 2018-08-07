96cd18a489a8b7750e65dfd6575f018cb60b1923
billie-badger <will.vatcher@gmail.com>
2018-08-07 16:50:56 -0700

    placeholder styling, no images, no copy

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -4,7 +4,17 @@ class Roadmap extends Component {
     render() {
         return (
             <div id='roadmap'>
-                
+                <h1>Roadmap</h1>
+                <div className='roadmap-inner'>
+                    <div className='hexagon' />
+                    <div className='hexagon' />
+                    <div className='hexagon' />
+                    <div className='hexagon' />
+                    <div className='hexagon' />
+                    <div className='hexagon' />
+                    <div className='hexagon' />
+                    <div className='hexagon' />
+                </div>
             </div>
         );
     }
--- src/components/Social.js
+++ src/components/Social.js
@@ -4,7 +4,12 @@ class Social extends Component {
     render() {
         return (
             <div id='social'>
-                
+                <h1>Social</h1>
+                <div className='social-inner'>
+                    <div className='twitter'></div>
+                    <div className='github'></div>
+                    <div className='instagram'></div>
+                </div>
             </div>
         );
     }
