a074950087c4c953e2a8fc579f466faa26b9e07e
billie-badger <will.vatcher@gmail.com>
2018-08-10 21:13:08 -0700

    Tweaking and updating

--- src/components/HeroSplash.js
+++ src/components/HeroSplash.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 import rowdy from '../assets/Runner_Rowdy_Standing.png';
 import bigboy from '../assets/PostApocalyptic_BigBoy_Standing.png';
+import Logo from './svg/Logo';
 
 class HeroSplash extends Component {
     render() {
         return (
             <div id='herosplash'>
-                <h1>Extreme Time Heroes</h1>
+                <Logo />
                 <img className='rowdy' src={rowdy} />
                 <img className='bigboy' src={bigboy} />
             </div>
