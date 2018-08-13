9facd2c4635a552c6b6e99cec1de2a57c7922d43
billie-badger <will.vatcher@gmail.com>
2018-08-13 09:22:08 -0700

    fixing alignments

--- src/components/Synopsis.js
+++ src/components/Synopsis.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Synopsis extends Component {
     render() {
         return (
+            
             <div id='synopsis'>
                 <h1>Synopsis</h1>
                 <p>Scientists have discovered increasing instability in time as we know it. Extensive studies by the most brilliant minds of our world have reached the hypothesis that time will eventually fracture into multiple, simultaneous timelines and realities. From this chaos, heroes will arise to defend their timelines and claim dominance above all.
@@ -10,6 +11,7 @@ class Synopsis extends Component {
                 <p>
                 Finality Labs’ Extreme Time Heroes is a strategic turn-based battle game. (Insert additional technical information here).</p>
             </div>
+            
         );
     }
 }
