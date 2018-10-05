51c5a47ef49230f721c7be5be56a334b0aa7ba0d
billie-badger <will.vatcher@gmail.com>
2018-10-04 22:55:15 -0700

    added logo, no animation yet

--- src/components/HeroSplash.js
+++ src/components/HeroSplash.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import header from '../assets/Header.svg';
+import logo from '../assets/logo/Logo animation_00149.png';
 
 class HeroSplash extends Component {
     render() {
         return (
             <div id='herosplash'>
                 <img src={header} width='100%' alt='header'/>
+                <img className='logo' src={logo} alt='logo'/>
             </div>
         );
     }
