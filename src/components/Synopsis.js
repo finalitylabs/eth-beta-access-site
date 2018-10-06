c60bdb4825eb72fe85c2d4498c0dbac595ebaf3b
nginnever <ginneversource@gmail.com>
2018-10-06 11:31:42 -0700

    text change

--- src/components/Synopsis.js
+++ src/components/Synopsis.js
@@ -9,9 +9,11 @@ class Synopsis extends Component {
                 <h1>SYNOPSIS</h1>
                 <p>Scientists have discovered increasing instability in time as we know it. Extensive studies by the most brilliant minds of our world have reached the hypothesis that time will eventually fracture into multiple, simultaneous timelines and realities. From this chaos, heroes will arise to defend their timelines and claim dominance above all.
                 </p>
+                <br/>
                 <img src={timeline} alt='Timeline' />
+                <br/>
                 <p className='synopsislastp'>
-                Finality Labs’ Eternal Time Heroes is a strategic turn-based battle game that uses blockchain based champions to assemble armies in search of glory. Before the battle begins, use the portal below to gain an early access token. Have some extra CryptoKitties laying around being lazy? Throw one into the time fracture and see what comes out!</p>
+                Finality Labs’ Eternal Time Heroes is a strategic turn-based battle game that uses blockchain-based champions to assemble armies in search of glory. Want in on the action? Use the portal below to gain an early access token. Have some extra CryptoKitties laying around being lazy? Throw one into the time fracture and see what comes out!</p>
             </div>
             
         );
