40eb3a48db8bfe67bcb3f2f2ef0514f1e415cb70
Jasper de Gooijer <j.degooijer@triple-it.nl>
2018-10-19 10:56:33 +0200

    made sure the terminal text is scrolling down on initial run and added the ETH logo to be animated in initial load

---
+++ src/components/animations/LogoAnimation.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+import logoAnimation from './../../assets/logo-animation.gif';
+import logo from './../../assets/logo/Logo animation_00149.png';
+
+class LogoAnimation extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            animationFinished: false
+        }
+    }
+    componentDidMount = () => {
+        window.setTimeout(()=>{
+            this.setState({
+                animationFinished: true
+            })
+        }, 4000)
+    }
+
+  render() {
+    return (
+        [
+            <img className='logo' src={logo} alt='logo'/>,
+            <img className={`logo ${!this.state.animationFinished ? "show" : "hide"}`} src={logoAnimation} alt="animated ETH logo"/>
+        ]
+    );
+  }
+}
+
+export default LogoAnimation;
