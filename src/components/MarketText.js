9542569fced11fc2200e17c25115761a04c17527
billie-badger <will.vatcher@gmail.com>
2018-10-22 19:59:55 -0700

    updated technical info 3 columns and roadmap

--- src/components/MarketText.js
+++ src/components/MarketText.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class MarketText extends Component {
     render() {
         return (
-            <div className='techtext'>
+            <div className='market-text'>
                 <h1>NFT MARKETPLACE</h1>
                 <p className='padding'>Having a place to express the value of your in-game objects is something that has been utilized in the past in systems like the WoW Auction House, CryptoKitties Auction Contracts, or centralized independent websites that host a place to buy and sell. In collaboration with District0x we are launching a Marketplace with E.T.H. that will operate in a completely decentralized way. Fees will go to paying those that maintain the Plasma market network, which we believe will reduce costs over centralized market places. The security of a trustless marketplace that can't be shut down is an added bonus.</p>
             </div>
